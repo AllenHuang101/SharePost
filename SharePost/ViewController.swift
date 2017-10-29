8d0f3bf2b61f7e11b6f85ec04afc83815b3f6d25
allen3_huang <allen3_huang@asus.com>
2017-10-29 10:44:26 +0800

    Initial Commit

---
+++ SharePost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SharePost
+//
+//  Created by allen3_huang on 2017/10/29.
+//  Copyright © 2017年 allen3_huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
